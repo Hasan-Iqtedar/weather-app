cd38b134d6a38550f1e475aa1ec955234c314ddc
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-01-25 19:42:13 +0500

    Add function to process received data

--- src/weather.js
+++ src/weather.js
@@ -1,14 +1,36 @@
 let weather = (() => {
   let apiKey = "d9f7ecce0fe14652d86845b06833e3ff";
 
+  let processData = (data) => {
+    let name = data.name;
+    let temperature = data.main.temp;
+    let feelsLike = data.main.feels_like;
+    let humidity = data.main.humidity;
+    let country = data.sys.country;
+    let weather = data.weather[0].description;
+    let windSpeed = data.wind.speed;
+
+    return {
+      name,
+      country,
+      weather,
+      temperature,
+      feelsLike,
+      humidity,
+      windSpeed,
+    };
+  };
+
   let getWeather = (location) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
-    console.log(url);
     fetch(url, {
       mode: "cors",
     })
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        console.log(data);
+        console.table(processData(data));
+      })
       .catch((err) => console.log("Error: " + err));
   };
 
